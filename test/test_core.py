d7071f22faa35a718588a531a8d66251d2ff2a6c
tjlane <thomas.joseph.lane@gmail.com>
2013-12-10 11:10:09 -0800

    added derivative test

--- test/test_core.py
+++ test/test_core.py
@@ -1,5 +1,35 @@
 
-# placeholder
-pass
+"""
+Tests for cxsinfer.core
+"""
 
+
+import numpy as np
+from numpy.testing import *
+import numdifftools as nd
+
+from cxsinfer import core
+
+
+class TestAutocorrRegressor(object):
+    
+    def setup(self):
+        
+        n_coefficients = 3 # this is lambdas
+        self.legendre_coefficients = np.abs(np.random.randn(n_coefficients))
+        self.ar = core.AutocorrRegressor(self.legendre_coefficients)
+        
+        return
+        
+    def test_analytical_gradient(self):
+
+        trials = 5
+
+        numerical_grad = nd.Gradient(self.ar._objective)
+        analytic_grad  = self.ar._objective_grad
+
+        x = self.ar.lbd_max**2 + 2*self.ar.lbd_max + 1
+        for i in range(trials):
+            g = np.random.randn(x*2)
+            assert_allclose(analytic_grad(g), numerical_grad(g))
 
